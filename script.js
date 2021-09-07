668bab1231e6f84fb909f2e8690517e5d62c5ff2
Nick Falbo <68254882+nfalbo213@users.noreply.github.com>
2021-09-07 16:26:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -632,7 +632,7 @@ const tankAI = () => {
                         roundFired = true;
                         //darkTankPosition[i].roundFiredIndex += 1;
                         tankRoundArr.push({x: darkTankPosition[n].x + 81, y: darkTankPosition[n].y + 17, velocity: 16, darkTankRound: true, roundLive: true, isBossRound: true});
-                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x + 81, darkTankPosition[n].y + 10, muzzleFire.width + 2, muzzleFire.height + 2);
+                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x + 81, darkTankPosition[n].y + 9, muzzleFire.width + 2, muzzleFire.height + 2);
                     }
                 }
                 if (darkTankPosition[n].roundFiredIndex === 3) {
@@ -657,7 +657,7 @@ const tankAI = () => {
                         roundFired = true;
                         //darkTankPosition[i].roundFiredIndex += 1;
                         tankRoundArr.push({x: darkTankPosition[n].x, y: darkTankPosition[n].y + 17, velocity: -16, darkTankRound: true, roundLive: true, isBossRound: true});
-                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x - 15, darkTankPosition[n].y + 12, muzzleFire.width + 2, muzzleFire.height + 2);
+                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x - 14, darkTankPosition[n].y + 9, muzzleFire.width + 2, muzzleFire.height + 2);
                     }
                 }
                 if (darkTankPosition[n].roundFiredIndex === 3) {
