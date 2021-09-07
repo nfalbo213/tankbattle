6600e9f3ae7e21d1b54ffd78d1fe68fe9a62a887
Nick Falbo <68254882+nfalbo213@users.noreply.github.com>
2021-09-07 16:34:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -657,7 +657,7 @@ const tankAI = () => {
                         roundFired = true;
                         //darkTankPosition[i].roundFiredIndex += 1;
                         tankRoundArr.push({x: darkTankPosition[n].x, y: darkTankPosition[n].y + 17, velocity: -16, darkTankRound: true, roundLive: true, isBossRound: true});
-                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x - 14, darkTankPosition[n].y + 9, muzzleFire.width + 2, muzzleFire.height + 2);
+                        battlefield_ctx.drawImage(muzzleFire, darkTankPosition[n].x - 15.5, darkTankPosition[n].y + 8, muzzleFire.width + 2, muzzleFire.height + 2);
                     }
                 }
                 if (darkTankPosition[n].roundFiredIndex === 3) {
